cd3c36c92f2ec74deebab32222c04d2a0be7b6c8
Evan Chiu <evan@evanchiu.com>
2016-03-31 16:41:56 -0700

    Making enemies blue dots

--- js/invaders.js
+++ js/invaders.js
@@ -5,7 +5,8 @@ function preload() {
 
     game.load.image('bullet', 'assets/bullet.png');
     game.load.image('enemyBullet', 'assets/enemy-bullet.png');
-    game.load.spritesheet('invader', 'assets/invader32x32x4.png', 32, 32);
+    //game.load.spritesheet('invader', 'assets/invader32x32x4.png', 32, 32);
+    game.load.image('invader', 'img/bluedot.png', 32, 32);
     game.load.image('ship', 'assets/player.png');
     game.load.spritesheet('kaboom', 'assets/explode.png', 128, 128);
     game.load.image('starfield', 'assets/starfield.png');
